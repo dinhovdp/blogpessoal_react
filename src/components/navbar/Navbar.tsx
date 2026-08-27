2e665d55929f58bbd09e488e7bdf59cfd5411486
Edson Silva <dinhovdp@gmail.com>
2026-08-26 21:46:08 -0300

    feat: Implementar funcionalidades de gerenciamento de postagens e temas
    - Adicionar componente DeletarPostagem para permitir a exclusão de postagens.
    - Criar FormPostagem para cadastro e edição de postagens.
    - Implementar ListaPostagens para listar todas as postagens.
    - Adicionar ModalPostagem para criar novas postagens em um modal.
    - Criar CardTema para exibir informações de temas.
    - Implementar DeletarTema para permitir a exclusão de temas.
    - Criar FormTema para cadastro e edição de temas.
    - Implementar ListaTemas para listar todos os temas.
    - Adicionar AuthContext para gerenciar autenticação de usuários.
    - Criar modelos para Postagem, Tema, Usuario e UsuarioLogin.
    - Implementar Cadastro e Login para gerenciamento de usuários.
    - Criar Service para interações com a API.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,21 +1,33 @@
-function Navbar(){
-    return(
+import {
+    GithubLogoIcon,
+    InstagramLogoIcon,
+    LinkedinLogoIcon,
+} from "@phosphor-icons/react";
 
-        <>
-        <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
-            <div className="container flex justify-between text-lg mx-8">
-                Blog Pessoal
-                <div className='flex gap-4'>
-                    Postagens
-                    Temas
-                    Cadastrar tema
-                    Perfil
-                    Sair
+function Footer() {
+    let data = new Date().getFullYear();
+
+    return (
+        <div className="flex justify-center bg-indigo-900 text-white">
+            <div className="container flex flex-col items-center py-4">
+                <p className="text-xl font-bold">
+                    Blog Pessoal Edson Silva | Copyright: {data}
+                </p>
+                <p className="text-lg">Acesse minhas redes sociais</p>
+                <div className="flex gap-2">
+                    <a href="#" target="_blank">
+                        <LinkedinLogoIcon size={48} weight="bold" />
+                    </a>
+                    <a href="#" target="_blank">
+                        <InstagramLogoIcon size={48} weight="bold" />
+                    </a>
+                    <a href="#" target="_blank">
+                        <GithubLogoIcon size={48} weight="bold" />
+                    </a>
                 </div>
             </div>
         </div>
-        </>
-    )
+    );
 }
 
-export default Navbar
+export default Footer;
