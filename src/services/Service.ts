64e672bfe94f9daf07b7dfe40eb2d0b0143b2f05
Edson Silva <dinhovdp@gmail.com>
2026-08-31 10:27:56 -0300

    Deploy do Projeto Projeto Blog Pessoal

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://blogpessoal-jkj7.onrender.com'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 // Função  cadastrar Usuario
